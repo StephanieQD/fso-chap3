2ef75e44679299ed93f98407e945a5bba3330276
StephanieQD <stephanie.qrys.deetman@gmail.com>
2023-08-08 15:47:38 -0500

    Complete exercise 3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
+
 app.use(express.json())
+app.use(cors())
 
 morgan.token("reqbody", function (req, res) {
   return JSON.stringify(req.body)
