51ce5a321aeaa2cdf97f2dabb12b4b0e081144f6
StephanieQD <stephanie.qrys.deetman@gmail.com>
2023-08-08 18:40:50 -0500

    Complete exercise 3.11 phonebook full stack

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Fullstackopen Chapter 03
 
 [View the deployed API here](https://fso-part03-phonebook.fly.dev/api/persons)
+
+[View the deployed frontend here](https://fso-part03-phonebook.fly.dev/)
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
+app.use(express.static('new_build'))
+
 morgan.token("reqbody", function (req, res) {
   return JSON.stringify(req.body)
 })
